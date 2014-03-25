4fa9e8bda0674489f7b10c17bb089ce470cfa198
abdotalaat <abdotalaat@abdotalaat-PC>
2014-03-25 20:09:19 +0200

    new commit

--- src/java/productsOperations/EditProduct.java
+++ src/java/productsOperations/EditProduct.java
@@ -32,16 +32,19 @@ public class EditProduct extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
+            
+            
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EditProduct</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EditProduct at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
         } finally {            
             out.close();
         }
